a7ffd12f1c3fe6a37e3b8aac3a06dc9f5a633147
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2024-09-20 08:58:28 -0600

    Don't hardcode root

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 set -e
 
-config_file="/home/root/.config/debian-chroot.conf"
+config_file="/home/$USER/.config/debian-chroot.conf"
 if [ -f "$config_file" ]; then
-  . "$config_file"
+  source "$config_file"
 fi
 repository="${repository:-"Eeems-Org/remarkable-debian-chroot"}"
 branch="${branch:-master}"
-bin_folder="${bin_folder:-"/home/root/.local/bin"}"
-chroot_path="${chroot_path:-"/home/root/.local/share/debian"}"
-debootstrap_path="${debootstrap_path:-"/home/root/.local/share/debootstrap"}"
+bin_folder="${bin_folder:-"/home/$USER/.local/bin"}"
+chroot_path="${chroot_path:-"/home/$USER/.local/share/debian"}"
+debootstrap_path="${debootstrap_path:-"/home/$USER/.local/share/debootstrap"}"
+debian_arch=${debian_arch:-armhf}
+debian_variant=${debian_variant:-minbase}
+debian_version=${debian_version:-bullseye}
 download() {
   if [ -f "$3" ]; then
     echo "Warning: ${3} already exists"
@@ -19,6 +22,10 @@ download() {
     --output-document="$3"
   chmod "$1" "$3"
 }
+SUDO=
+if [[ "$USER" != "root" ]] && command -v sudo > /dev/null; then
+  SUDO=sudo
+fi
 
 case "${1:-install}" in
   install)
@@ -41,19 +48,22 @@ branch="${branch}"
 bin_folder="${bin_folder}"
 chroot_path="${chroot_path}"
 debootstrap_path="${debootstrap_path}"
+debian_arch=${debian_arch}
+debian_variant=${debian_variant}
+debian_version=${debian_version}
 EOF
     "${bin_folder}/debian-chroot" true
     ;;
   uninstall)
-    rm -f "${bin_folder}/debian-chroot"
-    rm -f "${bin_folder}/debootstrap"
+    $SUDO rm -f "${bin_folder}/debian-chroot"
+    $SUDO rm -f "${bin_folder}/debootstrap"
     while grep -q "${chroot_path}" /proc/mounts; do
       grep "${chroot_path}" /proc/mounts \
       | sort -r \
       | cut -d' ' -f2 \
-      | xargs -rn1 umount -lqR
+      | xargs -rn1 $SUDO umount -lqR
     done
-    rm -rf "$chroot_path"
-    rm -f "$config_file"
+    $SUDO rm -rf "$chroot_path"
+    $SUDO rm -f "$config_file"
     ;;
 esac
